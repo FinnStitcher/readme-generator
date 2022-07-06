67abef260c944d1924013982d42774859e218358
Finn Stitcher <foundfootage@protonmail.com>
2022-07-06 15:45:07 -0700

    data validation and conditional rendering

--- index.js
+++ index.js
@@ -3,85 +3,106 @@ const inquirer = require("inquirer");
 
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
+// generic function for data validation
+const validate = (input) => {
+	if (input) {
+		return true;
+	} else {
+		console.log("This field is required.");
+		return false;
+	}
+};
+
 // array of questions for user input
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "Enter the title of your project."
-    },
+	{
+		type: "input",
+		name: "title",
+		message: "Enter the title of your project.",
+		validate: (input) => validate(input),
+	},
 	{
 		type: "input",
 		name: "description",
 		message: "Enter a description of your project.",
+		validate: (input) => validate(input),
 	},
 	{
 		type: "input",
 		name: "install",
 		message: "Describe how to install the project.",
+		validate: (input) => validate(input),
 	},
-    {
-        type: "list",
-        name: "license",
-        message: "Select the license you want to release your project under.",
-        choices: [
-            "Apache 2.0",
-            "CC0 1.0",
-            "CC BY 4.0",
-            "CC BY-NC 4.0",
-            "GNU GPL v3",
-            "ISC License",
-            "MIT License"
-        ]
-    },
-    {
-        type: "input",
-        name: "use",
-        message: "Provide any information about using this program that you wish to share."
-    },
-    {
-        type: "input",
-        name: "contribute",
-        message: "Provide contribution guidelines."
-    },
-    {
-        type: "input",
-        name: "test",
-        message: "Describe how to test this program after installing it."
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "Enter your GitHub username. This will be displayed in a 'Contact Me' section."
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter your email address. This will be displayed in a 'Contact Me' section."
-    }
+	{
+		type: "list",
+		name: "license",
+		message: "Select the license you want to release your project under.",
+		choices: [
+			"Apache 2.0",
+			"CC0 1.0",
+			"CC BY 4.0",
+			"CC BY-NC 4.0",
+			"GNU GPL v3",
+			"ISC License",
+			"MIT License",
+		],
+		validate: (input) => validate(input),
+	},
+	{
+		type: "input",
+		name: "use",
+		message:
+			"Provide any information about using this program that you wish to share.",
+		validate: (input) => validate(input),
+	},
+	{
+		type: "input",
+		name: "contribute",
+		message: "Provide contribution guidelines.",
+		validate: (input) => validate(input),
+	},
+	{
+		type: "input",
+		name: "test",
+		message: "Describe how to test this program after installing it.",
+	},
+	{
+		type: "input",
+		name: "github",
+		message:
+			"Enter your GitHub username. This will be displayed in a 'Contact Me' section.",
+	},
+	{
+		type: "input",
+		name: "email",
+		message:
+			"Enter your email address. This will be displayed in a 'Contact Me' section.",
+        validate: (input) => validate(input),
+	},
 ];
 
 // write README file
 function writeToFile(fileName, data) {
-    fs.writeFile('./dist/README.md', data, err => {
-        if (err) {
-            console.error(err)
-            return;
-        };
+	fs.writeFile("./dist/README.md", data, (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
 
-        console.log("File created. Look in the dist folder to see it.");
-    })
-};
+		console.log("File created. Look in the dist folder to see it.");
+	});
+}
 
 // initialize and run app
 function init() {
 	console.log(
 		"Welcome to the README Generator. In the following prompts, type '\\n' anywhere you want a linebreak in the final product."
 	);
-    
-    inquirer.prompt(questions)
-    .then(data => generateMarkdown(data))
-    .then(markdown => writeToFile("README", markdown));
+
+	inquirer
+		.prompt(questions)
+		.then((data) => generateMarkdown(data))
+		.then((markdown) => writeToFile("README", markdown));
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,58 +1,77 @@
 // [![Alt text](image source)](outgoing link)
 const licenses = {
-    "Apache 2.0": {
-        name: "Apache 2.0",
-        imgLink: "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
-        infoLink: "https://opensource.org/licenses/Apache-2.0"
-    },
-    "CC0 1.0": {
-        name: "CC0 1.0",
-        imgLink: "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg",
-        infoLink: "https://creativecommons.org/publicdomain/zero/1.0"
-    },
-    "CC BY 4.0": {
-        name: "CC BY 4.0",
-        imgLink: "https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg",
-        infoLink: "https://creativecommons.org/licenses/by/4.0"
-    },
-    "CC BY-NC 4.0": {
-        name: "CC BY-NC 4.0",
-        imgLink: "https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg",
-        infoLink: "https://creativecommons.org/licenses/by-nc/4.0"
-    },
-    "GNU GPL v3": {
-        name: "GPL v3",
-        imgLink: "https://img.shields.io/badge/License-GPLv3-blue.svg",
-        infoLink: "https://www.gnu.org/licenses/gpl-3.0"
-    },
-    "ISC License": {
-        name: "ISC",
-        imgLink: "https://img.shields.io/badge/License-ISC-blue.svg",
-        infoLink: "https://opensource.org/licenses/ISC"
-    },
-    "MIT License": {
-        name: "MIT",
-        imgLink: "https://img.shields.io/badge/License-MIT-yellow.svg",
-        infoLink: "https://opensource.org/licenses/MIT"
-    }
+	"Apache 2.0": {
+		name: "Apache 2.0",
+		imgLink: "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+		infoLink: "https://opensource.org/licenses/Apache-2.0",
+	},
+	"CC0 1.0": {
+		name: "CC0 1.0",
+		imgLink: "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg",
+		infoLink: "https://creativecommons.org/publicdomain/zero/1.0",
+	},
+	"CC BY 4.0": {
+		name: "CC BY 4.0",
+		imgLink: "https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg",
+		infoLink: "https://creativecommons.org/licenses/by/4.0",
+	},
+	"CC BY-NC 4.0": {
+		name: "CC BY-NC 4.0",
+		imgLink:
+			"https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg",
+		infoLink: "https://creativecommons.org/licenses/by-nc/4.0",
+	},
+	"GNU GPL v3": {
+		name: "GPL v3",
+		imgLink: "https://img.shields.io/badge/License-GPLv3-blue.svg",
+		infoLink: "https://www.gnu.org/licenses/gpl-3.0",
+	},
+	"ISC License": {
+		name: "ISC",
+		imgLink: "https://img.shields.io/badge/License-ISC-blue.svg",
+		infoLink: "https://opensource.org/licenses/ISC",
+	},
+	"MIT License": {
+		name: "MIT",
+		imgLink: "https://img.shields.io/badge/License-MIT-yellow.svg",
+		infoLink: "https://opensource.org/licenses/MIT",
+	},
 };
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (!license) {
+	if (!license) {
+		return "";
+	}
+
+	const selectedLicense = licenses[license];
+	const {name, imgLink, infoLink} = selectedLicense;
+
+	return `[![License: ${name}](${imgLink})](${infoLink})`;
+}
+
+function addTestingSection(text) {
+    if (text) {
+		return `## Testing
+        ${text}`;        
+    }
+	else {
         return '';
-    };
+	};
+}
 
-    const selectedLicense = licenses[license];
-    const {name, imgLink, infoLink} = selectedLicense;
-
-    return `[![License: ${name}](${imgLink})](${infoLink})`;
-};
+function addContact(github, email) {
+    if (email) {
+        return `For questions or further information, contact ${github} at ${email}.`
+    } else {
+        return `For questions or further information, reach out to ${data.email}.`
+    }
+}
 
 // generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+	return `# ${data.title}
 ${data.description}
 ${renderLicenseBadge(data.license)}
 
@@ -65,11 +84,10 @@ ${data.use}
 ## How to Contribute
 ${data.contribute}
 
-## Testing
-${data.test}
+${addTestingSection(data.test)}
 
 ## Questions
-For questions or further information, contact me at ${data.email}.`;
+${addContact(data.github, data.email)}`;
 }
 
 module.exports = generateMarkdown;
